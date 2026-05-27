a1a4dba274af05b4b7161b8f82a378d489bffc69
Gustavo Fontana Bertoldi <gustavobertoldi20@gmail.com>
2026-05-27 19:12:22 -0300

    criando mapa funcionando

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Konva from 'konva';
 import {gridInitializer} from './modules/grid.js'
 import { cameraInitializer } from './modules/camera.js';
-import {token_adm} from './modules/input.js'
+import {token_adm, map_adm} from './modules/input.js'
 
 const stage = new Konva.Stage({
   container: 'tabuleiro',
@@ -11,9 +11,18 @@ const stage = new Konva.Stage({
 });
 cameraInitializer(stage)
 
-const camadaMundo = new Konva.Layer();
-stage.add(camadaMundo);
+const low_layer = new Konva.Layer(); // Camada mais em baixo
+const middle_layer = new Konva.Layer(); // camada do meio
+const upper_layer = new Konva.Layer(); // camada superior
 
-gridInitializer(camadaMundo)
-token_adm(camadaMundo)
-camadaMundo.draw()
+stage.add(low_layer);
+stage.add(middle_layer);
+stage.add(upper_layer);
+
+map_adm(low_layer);
+gridInitializer(middle_layer);
+token_adm(upper_layer);
+
+low_layer.draw();
+middle_layer.draw();
+upper_layer.draw();
--- src/modules/input.js
+++ src/modules/input.js
@@ -2,7 +2,7 @@ import Konva from "konva";
 
 let selected_token = null;
 
-export function token_adm(camada){
+export function token_adm(layer){
     const button = document.getElementById('btn-add-token');
     button.addEventListener('click', () => {
         const circle = new Konva.Circle({
@@ -20,8 +20,8 @@ export function token_adm(camada){
             selected_token = circle;
         })
 
-        camada.add(circle);
-        camada.draw();
+        layer.add(circle);
+        layer.draw();
     })
 
     const btn_rmv = document.getElementById('btn-remove-token');
@@ -29,7 +29,27 @@ export function token_adm(camada){
         if(selected_token){
             selected_token.destroy();
             selected_token = null;
-            camada.draw();
+            layer.draw();
+        }
+    })
+}
+
+export function map_adm(layer){
+    const button = document.getElementById('btn-add-mapa');
+    button.addEventListener('click', () => {
+        const imgElement = new Image();
+        imgElement.src = './src/assets/maps/Screenshot_1.png';
+        imgElement.onload = () => {
+            const mapa = new Konva.Image({
+                image: imgElement, // <-- Passa o elemento HTML que acabou de carregar
+                x: window.innerWidth/2,
+                y: window.innerHeight/2,
+                width: 800,        // Largura inicial do mapa
+                height: 600,       // Altura inicial do mapa
+                draggable: false   // O mapa NÃO deve ser arrastável, quem arrasta é o stage!
+            });
+            layer.add(mapa);
+            layer.draw();
         }
     })
 }
